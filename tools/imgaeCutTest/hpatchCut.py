9692f049c715c1bdd1c51ad0bed5ba3c54c40f95
hongge831 <541324951@qq.com>
2018-01-30 15:18:32 +0800

    add some new code

--- tools/imgaeCutTest/hpatchCut.py
+++ tools/imgaeCutTest/hpatchCut.py
@@ -1,4 +1,5 @@
 #encoding=utf-8
+#这里用于自己数据的排列组合构造训练positive——pair和negtive-pair
 import cv2
 import numpy as np
 import pickle
--- tools/imgaeCutTest/makeData.py
+++ tools/imgaeCutTest/makeData.py
@@ -7,6 +7,10 @@ import sys,os
 from PIL import Image
 ##读取保存的列表文件
 totalList = pickle.load(open("./totalList.txt",'rb'))
+#patchInfo用于保存每一种场景有多少张图片以及每张图片有多少个patch
+patchInfo = []
+imageN = 0
+patchN = 0
 ##保存截取的结果列表到本地的totalList.txt文件
 fileSrc = "D:/PythonSpace/imgaeCutTest/data/"
 fileList = os.listdir(fileSrc)
@@ -15,13 +19,17 @@ for fileN,f in enumerate(fileList):
     cropList = totalList[fileN]
     for imgN,img in enumerate(os.listdir(imgSrc)):
         imgDir = imgSrc+img
-        imgTemp = Image.open(imgDir)
+        imgTemp = Image.open(imgDir).resize((640,640))
+        imageN+=1
         for ptcN, t in enumerate(cropList):
-            print(t)
-            imgTemp = imgTemp.crop(t)
-            imgTemp.save("./stored/"+f+'_'+str(imgN)+'_'+str(ptcN)+'.jpg')
-
-
+            imgTemp.crop(t).save("./stored/"+f+'_'+str(imgN)+'_'+str(ptcN)+'.jpg')
+            patchN+=1
+    patchInfo.append((imageN,patchN))
+    imageN = 0
+    patchN = 0
+    print("-->finish "+f)
+pickle.dump(patchInfo,open("./patchInfo.txt","wb"),True)
+print(patchInfo)
 
 
 
