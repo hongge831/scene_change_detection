f926e8e1847d702e8946d5c01b2d203204bcf0c8
hongge831 <541324951@qq.com>
2018-01-30 11:36:13 +0800

    add some new code

--- tools/imgaeCutTest/makeData.py
+++ tools/imgaeCutTest/makeData.py
@@ -10,9 +10,22 @@ totalList = pickle.load(open("./totalList.txt",'rb'))
 ##保存截取的结果列表到本地的totalList.txt文件
 fileSrc = "D:/PythonSpace/imgaeCutTest/data/"
 fileList = os.listdir(fileSrc)
-for f in fileList:
+for fileN,f in enumerate(fileList):
     imgSrc = fileSrc+f+'/'
-    
+    cropList = totalList[fileN]
+    for imgN,img in enumerate(os.listdir(imgSrc)):
+        imgDir = imgSrc+img
+        imgTemp = Image.open(imgDir)
+        for ptcN, t in enumerate(cropList):
+            print(t)
+            imgTemp = imgTemp.crop(t)
+            imgTemp.save("./stored/"+f+'_'+str(imgN)+'_'+str(ptcN)+'.jpg')
+
+
+
+
+
+
 
 
 
